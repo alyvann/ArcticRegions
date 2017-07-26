be3b2f3d29978f9d98a4c6215409573fcce445dd
alyvann <avann@stanford.edu>
2017-07-26 15:46:32 -0700

    Now support 10 essays.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+var essay_dict = {"essay1": {"top_image": "images/home_image.jpg", "article_content": "essay1.html"}, 
+				"essay2": {"top_image": "images/TARI_1_desolation.jpeg", "article_content": "essay2.html"},
+				"essay3": {"top_image": "images/TARI_2.Julianeshaab.jpg", "article_content": "essay3.html"},
+				"essay4": {"top_image": "images/TARI_3.Kakortok.jpg", "article_content": "essay4.html"},
+				"essay5": {"top_image": "images/TARI_4.1sermitsialik.jpg", "article_content": "essay5.html"},
+				"essay6": {"top_image": "images/TARI_5.karsut.jpg", "article_content": "essay6.html"},
+				"essay7": {"top_image": "images/TARI_6.devils.jpg", "article_content": "essay7.html"},
+				"essay8": {"top_image": "images/TARI_7.75N.jpg", "article_content": "essay8.html"},
+				"essay9": {"top_image": "images/TARI_8.godhavn.jpg", "article_content": "essay9.html"},
+				"essay10": {"top_image": "images/TARI_9.jacobshavn.jpg", "article_content": "essay10.html"}
+				};
+
 
 // Get the combined size of the navigation bar and top image
 function getHeaderSize(){
@@ -40,12 +52,6 @@ window.onscroll = function(event){
 
 
 function onPageButtonClick(e){
-	var dict = {"essay1": {"top_image": "images/TARI_1_desolation.jpeg", "article_content": "essay1.html"}, 
-				"essay2": {"top_image": "images/TARI_2.Julianeshaab.jpg", "article_content": "essay2.html"},
-				"essay3": {"top_image": "images/TARI_3.Kakortok.jpg", "article_content": "essay3.html"},
-				"essay4": {"top_image": "images/TARI_4.1sermitsialik.jpg", "article_content": "essay4.html"},
-				"essay5": {"top_image": "images/TARI_5.karsut.jpg", "article_content": "essay5.html"}};
-
 	var element_id = e.target.id;
 
 	if(element_id == "previous"){
@@ -58,11 +64,11 @@ function onPageButtonClick(e){
 
 	}else{	
 		// updates the top image to the image corresponding with the correct page
-		var new_image_src = dict[element_id]["top_image"];
+		var new_image_src = essay_dict[element_id]["top_image"];
 		document.getElementById("curr-img").src = new_image_src;
 
 		// load new article
-		var new_content_doc = dict[element_id]["article_content"];
+		var new_content_doc = essay_dict[element_id]["article_content"];
 		$("#content").load(new_content_doc); 
 
 	}
@@ -74,6 +80,9 @@ function onPageButtonClick(e){
 }
 
 function init(){
+	var first_essay_doc = essay_dict["essay1"]["article_content"];
+	$("#content").load(first_essay_doc); 
+
 	// initialize previous and next ??? 
 	// load first essay
 	// highlight the current page selected... 
