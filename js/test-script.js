4994cd04207fff2a28d3fe952dba2523c03699b9
Jose Recinto <jrecinto@stanford.edu>
2017-08-16 15:07:09 -0700

    Worked on test.html. Added map

--- js/test-script.js
+++ js/test-script.js
@@ -1,17 +1,25 @@
-var mymap = L.map('leafletmap');
-mymap.setView([40.775,-73.972], 15);
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-    maxZoom: 18
-}).addTo(mymap);
 
-$('.pure-button').on('click', function(){
-  mymap.locate({setView: true, maxZoom: 15});
-});
+    var map = L.map('map',{
+    center: [74.60068, -57.15637],
+    zoom: 7
+    });
 
-mymap.on('locationfound', onLocationFound);
-function onLocationFound(e) {
-    console.log(e); 
-    // e.heading will contain the user's heading (in degrees) if it's available, and if not it will be NaN. This would allow you to point a marker in the same direction the user is pointed. 
-    L.marker(e.latlng).addTo(mymap);
-}
+    var map2 = L.map('map2',{
+    center: [60.76586, -48.16131],
+    zoom: 7
+    });
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map2);
+
+
+    var marker = L.marker([74.60068, -57.15637]).addTo(map);
+    marker.bindPopup("<b>Devil's Thumb</b>").openPopup();
+
+    var markerTwo = L.marker([60.76586, -48.16131]).addTo(map2);
+    markerTwo.bindPopup("<b>Cape Desolation</b>").openPopup();
